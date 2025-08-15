b5969ffa206c3aba750a302ce03e11593e77364f
SAjiki <sergioajk@hotmail.com>
2025-08-15 13:22:36 -0400

    feat: criado rota health

---
+++ src/app/health/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Health = () => {
+    return (
+        <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontFamily: "Arial, sans-serif",
+        fontSize: "24px",
+      }}
+    >
+        Agenda SSD Front
+    </div>
+    );
+};
+export default Health;
