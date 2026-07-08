05a934281bd3171ee2d9eb96d3144f74665c9b36
SAjiki <sergioajk@hotmail.com>
2026-07-08 14:58:51 -0400

    feat: alteração de tamanho de fonte

--- src/components/calendar-layout/TopBar.tsx
+++ src/components/calendar-layout/TopBar.tsx
@@ -59,7 +59,7 @@ export default function TopBar({
       </div>
 
       <button className="btn-new-meeting" onClick={onNewMeeting}>
-        + Agendar Reunião
+        + Agendar
       </button>
 
       <div className="topbar-spacer" />
